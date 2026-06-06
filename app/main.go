b05e86f1eba3637058d305a1031726d1f06b6726
Victor <victorferrarihugo@hotmail.com>
2026-06-05 21:00:32 -0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,18 +14,58 @@ func main() {
 
 		command = command[:len(command)-1]
 
-		if command == "exit" {
-			os.Exit(0)
-		}
-
-		if strings.HasPrefix(command, "echo ") {
-			fmt.Println(command[5:])
-		} else {
-			fmt.Printf("%s: not found\n", command)
-		}
-
 		if err != nil {
 			os.Exit(1)
+		}
+
+		HandleCommand(command)
+	}
+}
+
+func HandleCommand(command string) {
+	prefix := strings.Split(command, " ")[0]
+
+	switch prefix {
+
+	case "exit":
+		{
+			os.Exit(1)
+		}
+
+	case "echo":
+		{
+			fmt.Println(command[5:])
+		}
+
+	case "type":
+		{
+			argument := strings.Split(command, " ")[1]
+			switch argument {
+
+			case "echo":
+				{
+					fmt.Printf("%s is a shell builtin\n", argument)
+				}
+
+			case "type":
+				{
+					fmt.Printf("%s is a shell builtin\n", argument)
+				}
+
+			case "exit":
+				{
+					fmt.Printf("%s is a shell builtin\n", argument)
+				}
+			default:
+				{
+					fmt.Printf("%s: not found\n", argument)
+				}
+			}
+		}
+
+	default:
+		{
+			fmt.Printf("%s: not found\n", command)
 		}
 	}
 }
